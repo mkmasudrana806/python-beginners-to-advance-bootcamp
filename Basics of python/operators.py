b91785dcb5ffa9d898fd3c379d3acc128ddd5597
mkmasudrana806 <mkmasudrana806@gmail.com>
2024-11-10 23:59:25 +0600

    learning arithmetic operators

---
+++ Basics of python/operators.py
@@ -0,0 +1,44 @@
+"""
+Operator in python
+1. Arithmetic Operators
+2. Relational Operators or comparision operators
+3. Assignment Operators
+4. Logical Operators
+5. Bitwise Operators
+6. Assignment Operators
+7. Membership Operators
+"""
+
+# Arithmetic Operatos are: +, -, *, /, %, **, =
+
+#   + 
+add = 10 + 20
+print(add)
+
+#   -
+sub = 10 - 20
+print(sub)
+
+# *
+mul = 10 * 20
+print(mul)
+
+# /
+div = 10 / 20
+print(div) # return quotient
+
+# % 
+a = 10
+b = 3
+remainder = a % b # return the remainder 
+print(remainder)
+
+# ** exponential operator
+exp = 2 ** 3 # similar to:  2 * 2 * 2
+print(exp)
+
+# // floor division
+a = 15
+b = 2
+ans = a // b
+print(ans) # return floor value that is 7
